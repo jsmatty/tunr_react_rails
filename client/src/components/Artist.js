917c537bd8f1462a885fa911f492f857854edae9
jsmatty <jscot.matty@gmail.com>
2017-09-05 14:47:31 -0400

    Added React Router and NavBar

---
+++ client/src/components/Artist.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Artist extends Component{
+  render(){
+    return (
+      <div>
+        <h1>
+          Hello from Artists!
+        </h1>
+      </div>
+    )
+  }
+}
+
+
+export default Artist
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class SignUp extends Component{
+  render(){
+    return (
+      <div>
+        <h1>
+          Hello from SignUp!
+        </h1>
+      </div>
+    )
+  }
+}
+
+
+export default SignUp
